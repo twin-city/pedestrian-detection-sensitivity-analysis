f394275ed49de7052c8c552503003afe7916f37a
RaphaelCouronne <raphael.couronne@gmail.com>
2023-03-15 19:01:52 +0100

    [FEAT] ECP metric computation

--- ECP_benchmark.py
+++ ECP_benchmark.py
@@ -1,53 +1,45 @@
 
 
-
-
-
-"""
-For now inference detector is launched multiple times
-
-
-- How to save ?
-
-- Will be accelerated later with
-
-./tools/dist_test.sh \
-    configs/mask_rcnn_r50_fpn_1x_coco.py \
-    checkpoints/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth \
-    8 \
-    --out results.pkl \
-    --eval bbox segm
-"""
-
-
 import os
 import numpy as np
 import torch
 import json
 
-#%% Which images to apply on ? And annotations For now Caltech Pedestrian
+#%% Load the ECP images
+
+lyon_day_root = "/media/raphael/Projects/datasets/EuroCityPerson/ECP/day/labels/val/lyon"
+lyon_day_json_files = [x for x in os.listdir(lyon_day_root) if ".json" in x]
+lyon_day_frame_ids = [x.split("_")[1].split(".json")[0] for x in os.listdir(lyon_day_root) if ".json" in x]
 
 
-# Load Caltech annotations
-json_path = "/home/raphael/work/code/dataset_conversion_utils/caltech-pedestrian-dataset-converter/data/annotations.json"
-with open(json_path) as jsonFile:
-    annot_caltech = json.load(jsonFile)
 
 
-# All targets in set01/V000 of Caltech Pedestrian to torch format
+
 targets = {}
-for frame_id, frame in annot_caltech['set01']["V000"]["frames"].items():
-    target = [
-      dict(
-        boxes=torch.tensor([(b["pos"][0], b["pos"][1], b["pos"][0] + b["pos"][2], b["pos"][1] + b["pos"][3])
-     for b in annot_caltech['set01']["V000"]["frames"][frame_id]]),
-        labels=torch.tensor([0]*len(annot_caltech['set01']["V000"]["frames"][frame_id])),
-      )
-    ]
-    targets[frame_id] = target
+for frame_id in lyon_day_frame_ids:
 
-# Which images to work on ?
-frame_id_list = list(annot_caltech['set01']["V000"]["frames"].keys())[:100]
+    # Load ECP annotations
+    json_path = f"/media/raphael/Projects/datasets/EuroCityPerson/ECP/day/labels/val/lyon/lyon_{frame_id}.json"
+    with open(json_path) as jsonFile:
+        annot_ECP = json.load(jsonFile)
+
+        target = [
+          dict(
+            boxes=torch.tensor(
+                [(c["x0"], c["x1"], c["y0"], c["y1"]) for c in annot_ECP["children"] if c["identity"]=="pedestrian"]
+            ),
+          )
+        ]
+
+        target[0]["labels"] = torch.tensor([0]*len(target[0]["boxes"]))
+
+        targets[frame_id] = target
+
+#%% Which images to apply on ? And annotations For now Caltech Pedestrian
+frame_id_list = lyon_day_frame_ids[:100]
+
+
+
 
 #%% Perform the inferences
 
@@ -61,16 +53,15 @@ checkpoint_file = "/home/raphael/work/checkpoints/detection/faster_rcnn_r50_fpn_
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cpu')
 
-os.makedirs("out/caltech_pedestrian", exist_ok=True)
+os.makedirs("out/ECP", exist_ok=True)
 
 for frame_id in frame_id_list:
     print(frame_id)
 
-    out_file = f"out/caltech_pedestrian/results_{frame_id}.npy"
+    out_file = f"out/ECP/results_{frame_id}.npy"
 
     if not os.path.exists(out_file):
-        img_path = "/home/raphael/work/code/dataset_conversion_utils/caltech-pedestrian-dataset-converter/data/images/set01_V000_{}.png".format(
-            frame_id)
+        img_path = f"/media/raphael/Projects/datasets/EuroCityPerson/ECP/day/img/val/lyon/lyon_{frame_id}.png"
 
         # test a single image and show the results
         result = inference_detector(model, img_path)
@@ -87,7 +78,7 @@ from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
 preds = {}
 for frame_id in frame_id_list:
-    bboxes_people = np.load(f"out/caltech_pedestrian/results_{frame_id}.npy")
+    bboxes_people = np.load(f"out/ECP/results_{frame_id}.npy")
     pred = [
       dict(
         boxes=torch.stack([torch.tensor(bbox[:4]) for bbox in bboxes_people], axis=0),
@@ -154,3 +145,58 @@ print(results)
 
 
 #%% Check si cohérent avec les images !!!!!!
+
+
+"""
+Here brief analysis : probably because of groups of people, or NMS, too many boxes
+"""
+
+import matplotlib.pyplot as plt
+import cv2
+
+frame_id_max_missrate = np.argmax([x[1] for x in results.values()])
+frame_id_max_fp = np.argmax([x[0] for x in results.values()])
+
+
+frame_id = lyon_day_frame_ids[frame_id_max_fp]
+frame_id = lyon_day_frame_ids[frame_id_max_missrate] #todo pas ouf annot ici
+
+# load img and plot results
+def add_bboxes_to_img(img, bboxes, c=(0,0,255), s=1):
+    for bbox in bboxes:
+        x1, y1, x2, y2 = [int(v) for v in bbox]
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), c, s)
+    return img
+
+def plot_results_img(frame_id, preds, targets, s=1):
+    img_path = f"/media/raphael/Projects/datasets/EuroCityPerson/ECP/day/img/val/lyon/lyon_{frame_id}.png"
+    img = plt.imread(img_path)
+    img = add_bboxes_to_img(img, preds[frame_id][0]["boxes"], c=(0, 0, 255), s=s)
+    img = add_bboxes_to_img(img, targets[frame_id][0]["boxes"], c=(0, 255, 0), s=s)
+    plt.imshow(img)
+    plt.show()
+
+plot_results_img(frame_id, preds, targets)
+
+
+#%% Check how the bboxes were attributed
+import matplotlib.pyplot as plt
+import cv2
+import torch
+frame_id = "00218"
+
+img_path = f"/media/raphael/Projects/datasets/EuroCityPerson/ECP/day/img/val/lyon/lyon_{frame_id}.png"
+img = plt.imread(img_path)
+#img = add_bboxes_to_img(img, preds[frame_id][0]["boxes"], c=(0, 0, 255))
+
+index_matched = torch.tensor(results[frame_id][2])
+index_missed = torch.tensor(results[frame_id][3])
+
+img = add_bboxes_to_img(img, targets[frame_id][0]["boxes"][index_matched], c=(0, 255, 0), s=2)
+img = add_bboxes_to_img(img, targets[frame_id][0]["boxes"][index_missed], c=(255, 0, 0), s=2)
+img = add_bboxes_to_img(img, preds[frame_id][0]["boxes"], c=(0, 0, 255), s=1)
+
+plt.imshow(img)
+plt.show()
+
+# todo Debug the ECP Benchmark ici !!! et choper des métriques déjà calculer pourcheck
